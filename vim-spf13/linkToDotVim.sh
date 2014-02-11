4d2c45ee8b888122a97740c6aaf00bd9a3fccfb1
npl-rig5 <npl.rig5@gmail.com>
2014-02-11 10:36:52 -0800

    vim-spf13 fixes

--- vim-spf13/linkToDotVim.sh
+++ vim-spf13/linkToDotVim.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+# install spf13
+curl http://j.mp/spf13-vim3 -L -o - | sh
+
 rm ~/.vimrc.local
 rm ~/.vimrc.before.local
 rm ~/.vimrc.bundles.local
 
+# copy modifying files
 THIS="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 ln $THIS/vimrc.local ~/.vimrc.local
 ln $THIS/vimrc.before.local ~/.vimrc.before.local
 ln $THIS/vimrc.bundles.local ~/.vimrc.bundles.local
 
+# update vim
 vim +BundleInstall +BundleClean
 
